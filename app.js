eb48c4807e3e90fe60dca69553141d1dc1e68989
HackHusky <kurst811@gmail.com>
2016-04-29 23:25:21 -0700

    robot files

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ maestro.on("ready", function() { // Set servo speed and some servo position
  maestro.setSpeed(11, 60);
  maestro.setSpeed(12, 60);
  maestro.setSpeed(13, 60);
- maestro.setTarget(12, 1200); 
- maestro.setTarget(13, 1800);
+
 });
 // Http handler function
 function handler (req, res) {
@@ -82,93 +81,9 @@ function handler (req, res) {
   var ForwardServo11 = [6217, 6217, 6217, 8081, 5984, 5984, 6056, 5984, 5984];
   var ForwardServo12 = [9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323];
 
-  var ReverseServo1  = [6001, 8024, 8024, 8024, 8024, 8024, 8024, 8024, 8024, 8024, 8024, 6001];
-  var ReverseServo2  = [3862, 6030, 3772, 3772, 3772, 3772, 3772, 3772, 3772, 3772, 3772, 3862];
-  var ReverseServo3  = [9086, 9086, 9086, 9086, 9086, 9086, 9086, 9086, 9086, 9086, 9086, 9086];
-  var ReverseServo4  = [6561, 6561, 6561, 6561, 6561, 6561, 6561, 6561, 6561, 9479, 9479, 6561];
-  var ReverseServo5  = [3293, 3293, 3293, 3293, 3293, 3293, 3293, 3293, 3293, 3293, 3293, 3293];
-  var ReverseServo6  = [6721, 6721, 6721, 6721, 6721, 6754, 6754, 6754, 6754, 6754, 6754, 6721];
-  var ReverseServo7  = [6144, 6144, 6144, 6144, 6144, 6144, 6144, 8442, 8442, 8442, 8442, 6144];
-  var ReverseServo8  = [8624, 8624, 8624, 8624, 8624, 8624, 8624, 6202, 9227, 9227, 9227, 8624];
-  var ReverseServo9  = [5051, 5051, 5051, 5051, 5051, 5051, 5051, 5051, 5096, 5096, 5096, 5051];
-  var ReverseServo10 = [6173, 6173, 6173, 6173, 6173, 7744, 7744, 7744, 7744, 7744, 7744, 6173];
-  var ReverseServo11 = [6217, 6217, 6217, 6217, 6217, 8005, 5707, 5707, 5707, 5707, 5707, 6217];
-  var ReverseServo12 = [9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323];
-
-  var UnverseServo1  = [5193, 5193, 5193, 5193, 5193, 5193, 5193, 3039, 5193];
-  var UnverseServo2  = [3862, 3862, 3862, 3862, 3862, 3862, 3862, 3862, 3862];
-  var UnverseServo3  = [9086, 9165, 9007, 9007, 9007, 9007, 9007, 9007, 9007];
-  var UnverseServo4  = [7356, 5148, 4441, 4441, 4441, 4441, 4441, 4441, 7356];
-  var UnverseServo5  = [3293, 3293, 2933, 2933, 2933, 2933, 2933, 2933, 3293];
-  var UnverseServo6  = [6721, 3904, 6929, 6929, 6929, 6929, 6929, 6929, 6721];
-  var UnverseServo7  = [5906, 5906, 5906, 5906, 5906, 4660, 4660, 4660, 5906];
-  var UnverseServo8  = [8624, 8624, 8624, 8624, 8624, 5794, 8907, 8907, 8624];
-  var UnverseServo9  = [5051, 5051, 5051, 5051, 5051, 5051, 5051, 5051, 5051];
-  var UnverseServo10 = [6411, 6411, 6411, 5184, 4194, 4194, 4194, 4194, 6411];
-  var UnverseServo11 = [6217, 6217, 6217, 7802, 5736, 5736, 5736, 5736, 6217];
-  var UnverseServo12 = [9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323, 9323];
-
-  var ServoTransform1  = [8933, 8933, 8933, 8933, 8933, 8933, 8933, 8933, 8933];  
-  var ServoTransform2  = [3508, 1984, 0,0,0,0,0,0,0];
-  var ServoTransform3  = [8858, 10944,0,0,0,0,0,0,0];
-  var ServoTransform4  = [3704, 3704, 3704, 3704, 3704, 3704, 3704, 3704, 3704];
-  var ServoTransform5  = [3218, 3218, 3218, 3218, 3218, 3218, 3218, 2047, 0];
-  var ServoTransform6  = [6580, 6580, 6580, 6580, 6580, 6580, 6580, 8296, 0];
-  var ServoTransform7  = [3467, 3467, 3467, 3467, 3467, 3467, 3467, 3467, 3467];
-  var ServoTransform8  = [8921, 8921, 8921, 8921, 8921, 10816, 10816, 10816,0];
-  var ServoTransform9  = [5154, 5154, 5154, 5154, 5154, 4398, 4078, 4078,0];
-  var ServoTransform10 = [8878, 8878, 8878, 8878, 8878, 8878, 8878, 8878,0];
-  var ServoTransform11 = [6202, 6202, 6202, 3380,0,0,0,0,0];
-  var ServoTransform12 = [9055, 9055, 9055, 11200,0,0,0,0,0]; 
-
-
 
 /*-----------------------------------------------------------------Servo Function----------------------------------------------------------------*/
-function CameraRight(){
-    yaw = yaw+100;
-    maestro.setTarget(12, yaw); 
-}
-function CameraLeft(){
-    yaw = yaw-100;
-    maestro.setTarget(12, yaw); 
-}
-function CameraUp(){
-    pitch = pitch+100;
-    maestro.setTarget(13, pitch); 
-}
-function CameraDown(){
-    pitch = pitch-100;
-    maestro.setTarget(13, pitch); 
-}
-function Rest(){
-    //Bot Rest postion
-    maestro.setTarget(0, 1900);
-    maestro.setTarget(1, 700); 
-    maestro.setTarget(2, 800);
-    maestro.setTarget(3, 1350); 
-    maestro.setTarget(4, 2300); 
-    maestro.setTarget(5, 2250);
-    maestro.setTarget(6, 1300);  
-    maestro.setTarget(7, 1800); 
-    maestro.setTarget(8, 2300);
-    maestro.setTarget(9, 1700);
-    maestro.setTarget(10, 1200); 
-    maestro.setTarget(11, 1300);
-}     
-function Idle(){
-    maestro.setTarget(0, 1611);
-    maestro.setTarget(1, 950); 
-    maestro.setTarget(2, 2300);
-    maestro.setTarget(3, 1552); 
-    maestro.setTarget(4, 800); 
-    maestro.setTarget(5, 1700);
-    maestro.setTarget(6, 1500);  
-    maestro.setTarget(7, 2150); 
-    maestro.setTarget(8, 1300);
-    maestro.setTarget(9, 1622);
-    maestro.setTarget(10, 1570); 
-    maestro.setTarget(11, 2280); 
-}
+
 function Forward(){
   var Delay = 500;
   var Count = 0;
@@ -190,96 +105,9 @@ function Forward(){
     }, i*Delay);
   }    
 }
-function Reverse(){
-  var Delay = 500;
-  var Count = 0;
-  for (var i=0;i<=11;i++){
-    setTimeout(function(){
-      maestro.setTarget(0,  (ReverseServo1[Count]/4.02));
-      maestro.setTarget(1,  (ReverseServo2[Count]/4.02)); 
-      maestro.setTarget(2,  (ReverseServo3[Count]/4.02));
-      maestro.setTarget(3,  (ReverseServo4[Count]/4.02)); 
-      maestro.setTarget(4,  (ReverseServo5[Count]/4.02)); 
-      maestro.setTarget(5,  (ReverseServo6[Count]/4.02));
-      maestro.setTarget(6,  (ReverseServo7[Count]/4.02));  
-      maestro.setTarget(7,  (ReverseServo8[Count]/4.02)); 
-      maestro.setTarget(8,  (ReverseServo9[Count]/4.02));
-      maestro.setTarget(9,  (ReverseServo10[Count]/4.02));
-      maestro.setTarget(10, (ReverseServo11[Count]/4.02)); 
-      maestro.setTarget(11, (ReverseServo12[Count]/4.02));
-      Count++;
-    }, i*Delay);
-  }    
-}
-function Unverse(){
-  var Delay = 500;
-  var Count = 0;
-  for (var i=0;i<=8;i++){
-    setTimeout(function(){
-      maestro.setTarget(0,  (UnverseServo1[Count]/4.02));
-      maestro.setTarget(1,  (UnverseServo2[Count]/4.02)); 
-      maestro.setTarget(2,  (UnverseServo3[Count]/4.02));
-      maestro.setTarget(3,  (UnverseServo4[Count]/4.02)); 
-      maestro.setTarget(4,  (UnverseServo5[Count]/4.02)); 
-      maestro.setTarget(5,  (UnverseServo6[Count]/4.02));
-      maestro.setTarget(6,  (UnverseServo7[Count]/4.02));  
-      maestro.setTarget(7,  (UnverseServo8[Count]/4.02)); 
-      maestro.setTarget(8,  (UnverseServo9[Count]/4.02));
-      maestro.setTarget(9,  (UnverseServo10[Count]/4.02));
-      maestro.setTarget(10, (UnverseServo11[Count]/4.02)); 
-      maestro.setTarget(11, (UnverseServo12[Count]/4.02));
-      Count++;
-    }, i*Delay);
-  }    
-}
 
-function Transfrom(){
-  var Delay = 500;
-  var Count = 0;
-  for (var i=0;i<=8;i++){
-    setTimeout(function(){
-      maestro.setTarget(0,  (ServoTransform1[Count]/4.02));
-      maestro.setTarget(1,  (ServoTransform2[Count]/4.02)); 
-      maestro.setTarget(2,  (ServoTransform3[Count]/4.02));
-      maestro.setTarget(3,  (ServoTransform4[Count]/4.02)); 
-      maestro.setTarget(4,  (ServoTransform5[Count]/4.02)); 
-      maestro.setTarget(5,  (ServoTransform6[Count]/4.02));
-      maestro.setTarget(6,  (ServoTransform7[Count]/4.02));  
-      maestro.setTarget(7,  (ServoTransform8[Count]/4.02)); 
-      maestro.setTarget(8,  (ServoTransform9[Count]/4.02));
-      maestro.setTarget(9,  (ServoTransform10[Count]/4.02));
-      maestro.setTarget(10, (ServoTransform11[Count]/4.02)); 
-      maestro.setTarget(11, (ServoTransform12[Count]/4.02));
-      Count++;
-    }, i*Delay);
-  }    
-}
 
-/*------------------------------------------------------------------Motor Function ------------------------------------------------------------------*/
-function MotorForward(){
-   PythonShell.run('MotorForward.py', function (err) {
-  if (err) throw err;
-  console.log('finished');
-  });
-}
-function MotorBackward(){
-   PythonShell.run('MotorBackward.py', function (err) {
-  if (err) throw err;
-  console.log('finished');
-  });
-}
-function MotorRight(){
-   PythonShell.run('MotorRight.py', function (err) {
-  if (err) throw err;
-  console.log('finished');
-  });
-}
-function MotorLeft(){
-   PythonShell.run('MotorLeft.py', function (err) {
-  if (err) throw err;
-  console.log('finished');
-  });
-}
+
 /*-------------------------------------------------------------------Handling Request----------------------------------------------------------------*/
 // Web Socket Connection
 io.sockets.on('connection', function (socket) {
@@ -324,44 +152,7 @@ io.sockets.on('connection', function (socket) {
           socket.emit("R");
       }, 1000);
   });
-  socket.on('CR', function(data) {
-      console.log("CameraRight");
-      CameraRight();   
-  });
-   socket.on('CL', function(data) {
-      console.log("CameraLeft");
-      CameraLeft();   
-  });
-    socket.on('CU', function(data) {
-      console.log("CameraUp");
-      CameraUp();   
-  });
-     socket.on('CD', function(data) {
-      console.log("CameraDown");
-      CameraDown();   
-  });
-  socket.on('MF', function(data) {
-      console.log("MotorForward");
-      MotorForward();
-  });
-  socket.on('MB', function(data) {
-      console.log("CameraDown");
-      MotorBackward();  
-  });
-
-   socket.on('MR', function(data) {
-      console.log("MotorRight");
-      MotorRight();  
-  });
-    socket.on('ML', function(data) {
-      console.log("MotorLeft");
-      MotorLeft();  
-  });  
-
-   socket.on('DR', function(data) {
-      console.log("CameraDown");
-      Transfrom();  
-  });
+  
 
 /* setInterval(function(){
     //console.log("sending compass info");
--- public/js/client.js
+++ public/js/client.js
@@ -24,87 +24,31 @@ socket.on('R', function (data) {
     console.log("Feedback: Right command received");
 });
 
-function hoveroff(){
-     $("#idle").css("background-color","#3498DB","opacity","50%");
-     $("#rest").css("background-color","#3498DB","opacity","50%");
-     $("#tank").css("background-color","#3498DB","opacity","50%");
-}
+
 
 $(document).keydown(function(e){ // pressing key 
 
    var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
 
-if(key == 111){ // r
-        socket.emit('CU');
-    }
-if(key == 108){ // r
-        socket.emit('CD');
-    }
-if(key== 107){ // r
-        socket.emit('CR');
-    }
-if(key == 59){ // r
-        socket.emit('CL');
-    }
-if(key == 82){ // r
-      $("#rest").css("background-color","#81CFE0","opacity","50%");
-      $("#idle").css("background-color","#3498DB","opacity","50%");
-      $("#walking").css("background-color","#3498DB","opacity","50%");
-      socket.emit('Rest');    
-}
-if(key == 73){ // i
-      $("#idle").css("background-color","#81CFE0","opacity","50%");
-      $("#rest").css("background-color","#3498DB","opacity","50%");
-      $("#walking").css("background-color","#3498DB","opacity","50%");
-         socket.emit('Idle');
-}
+
 if(key == 87){ // w 
-    $("#d-up").css("background-color","#404d4d","opacity","50%"); // hover effect 
-    $("#walking").css("background-color","#81CFE0","opacity","50%");
-    hoveroff();
+ 
         socket.emit('F');
     }
 if(key == 65){ // a 
-    $("#d-left").css("background-color","#404d4d","opacity","50%");
-    $("#walking").css("background-color","#81CFE0","opacity","50%");
-    hoveroff();
+
       socket.emit('L');
     }
 if(key == 68){ // d 
-    $("#d-right").css("background-color","#404d4d","opacity","50%");
-    $("#walking").css("background-color","#81CFE0","opacity","50%");
-    hoveroff();
+
       socket.emit('R');
     }
 if(key == 83){ // s 
-    $("#d-down").css("background-color","#404d4d","opacity","50%");
-    $("#walking").css("background-color","#81CFE0","opacity","50%");
-    hoveroff();
+
       socket.emit('B');
     }
-if(key == 38){//up key
-    socket.emit('MF');
-    }
-if(key == 40){//down key
-    socket.emit('MB');
-    }
-if(key == 39){//right key
-    socket.emit('MR');
-    }
-if(key == 37){//left key
-    socket.emit('ML');
-    }           
-if(key == 89){//down key
-    socket.emit('DR');
-    }        
+       
 });
-
-$(document).keyup(function(e){  
-    $("#d-up").css("background-color"," #1d2323");
-    $("#d-down").css("background-color"," #1d2323");
-    $("#d-left").css("background-color"," #1d2323");
-    $("#d-right").css("background-color"," #1d2323");
-  });
 
 
 
